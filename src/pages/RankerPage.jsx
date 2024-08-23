bf4b0b06a9b7f84f63e8da1d137dd3c2f3a07f8a
Matthieu MIGNE <matthieu.migne@orange.fr>
2024-08-23 17:23:16 +0200

    ranking page first clean implementation

--- src/pages/RankerPage.jsx
+++ src/pages/RankerPage.jsx
@@ -1,9 +1,73 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import CommonLayout from '../components/generic/CommonLayout';
 import RankingOverview from '../components/RankingOverview';
 import '../styles/RankerPage.css';
+
+const SongsToRank = ({ songsToRank, layout }) => (
+    <Row className={`songs-to-rank-row ${layout}`}>
+        <Col xs={6} md={4} className="song-to-rank">
+            <h5>{songsToRank[0].name}</h5>
+            <p>{songsToRank[0].artists.join(', ')}</p>
+        </Col>
+        <Col xs={6} md={4} className="song-to-rank">
+            <h5>{songsToRank[1].name}</h5>
+            <p>{songsToRank[1].artists.join(', ')}</p>
+        </Col>
+    </Row>
+);
+
+SongsToRank.propTypes = {
+    songsToRank: PropTypes.arrayOf(
+        PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            artists: PropTypes.arrayOf(PropTypes.string).isRequired
+        })
+    ).isRequired,
+    layout: PropTypes.string.isRequired
+};
+
+const WideScreenLayout = ({ songsToRank }) => (
+    <Row className="wide-screen-layout">
+        <Col md={4} className="songs-to-rank-col">
+            <SongsToRank songsToRank={songsToRank} layout="wide" />
+        </Col>
+        <Col md={8}>
+            <RankingOverview />
+        </Col>
+    </Row>
+);
+
+WideScreenLayout.propTypes = {
+    songsToRank: PropTypes.arrayOf(
+        PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            artists: PropTypes.arrayOf(PropTypes.string).isRequired
+        })
+    ).isRequired
+};
+
+const SmallScreenLayout = ({ songsToRank }) => (
+    <Col>
+        <SongsToRank songsToRank={songsToRank} layout="small" />
+        <Row>
+            <Col className="ranking-overview-col">
+                <RankingOverview />
+            </Col>
+        </Row>
+    </Col>
+);
+
+SmallScreenLayout.propTypes = {
+    songsToRank: PropTypes.arrayOf(
+        PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            artists: PropTypes.arrayOf(PropTypes.string).isRequired
+        })
+    ).isRequired
+};
 
 const RankerPage = () => {
     const [songsToRank] = useState([
@@ -14,33 +78,12 @@ const RankerPage = () => {
     return (
         <CommonLayout>
             <Container fluid className="ranker-page">
-                <Row>
-                    <Col md={4} className="d-none d-md-block">
-                        <div className="song-to-rank mb-4">
-                            <h5>{songsToRank[0].name}</h5>
-                            <p>{songsToRank[0].artists.join(', ')}</p>
-                        </div>
-                        <div className="song-to-rank">
-                            <h5>{songsToRank[1].name}</h5>
-                            <p>{songsToRank[1].artists.join(', ')}</p>
-                        </div>
-                    </Col>
-                    <Col xs={12} className="d-block d-md-none">
-                        <Row>
-                            <Col xs={6} className="song-to-rank">
-                                <h5>{songsToRank[0].name}</h5>
-                                <p>{songsToRank[0].artists.join(', ')}</p>
-                            </Col>
-                            <Col xs={6} className="song-to-rank">
-                                <h5>{songsToRank[1].name}</h5>
-                                <p>{songsToRank[1].artists.join(', ')}</p>
-                            </Col>
-                        </Row>
-                    </Col>
-                    <Col md={8} xs={12}>
-                        <RankingOverview />
-                    </Col>
-                </Row>
+                <div className="d-none d-md-block">
+                    <WideScreenLayout songsToRank={songsToRank} />
+                </div>
+                <div className="d-block d-md-none">
+                    <SmallScreenLayout songsToRank={songsToRank} />
+                </div>
             </Container>
         </CommonLayout>
     );
