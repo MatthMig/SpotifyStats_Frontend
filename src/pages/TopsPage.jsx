7852e23aca201f99c2afbf0e8c7a2373c0d7bcc2
Matthieu MIGNE <matthieu.migne@orange.fr>
2024-09-05 15:26:06 +0200

    tops page responsive

--- src/pages/TopsPage.jsx
+++ src/pages/TopsPage.jsx
@@ -1,12 +1,72 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import PropTypes from 'prop-types';
 import React, { useEffect, useRef, useState } from 'react';
-import { Col } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import { FaArrowRight } from 'react-icons/fa';
 import MostPlayedArtists from '../components/MostPlayedArtists';
 import MostPlayedSongs from '../components/MostPlayedSongs';
 import CommonLayout from '../components/generic/CommonLayout';
 import '../styles/HomePage.css';
 import '../styles/styles.css';
+
+const WideScreenLayout = ({ scrollContainerRef, handleClick, arrowVisible }) => (
+    <>
+        <div ref={scrollContainerRef} className="scroll-container d-flex overflow-auto no-padding">
+            <Col xs={12} md={4} className="d-flex flex-column overflow-auto mb-3 mb-md-0">
+                <MostPlayedSongs timeRange="short_term" />
+            </Col>
+            <Col xs={12} md={4} className="d-flex flex-column overflow-auto mb-3 mb-md-0">
+                <MostPlayedSongs timeRange="medium_term" />
+            </Col>
+            <Col xs={12} md={4} className="d-flex flex-column overflow-auto mb-3 mb-md-0">
+                <MostPlayedSongs timeRange="long_term" />
+            </Col>
+            <Col xs={12} md={4} className="d-flex flex-column overflow-auto mb-3 mb-md-0">
+                <MostPlayedArtists timeRange="short_term" />
+            </Col>
+            <Col xs={12} md={4} className="d-flex flex-column overflow-auto mb-3 mb-md-0">
+                <MostPlayedArtists timeRange="medium_term" />
+            </Col>
+            <Col xs={12} md={4} className="d-flex flex-column overflow-auto">
+                <MostPlayedArtists timeRange="long_term" />
+            </Col>
+        </div>
+        {arrowVisible && (
+            <div className="arrow-right" onClick={handleClick}>
+                <FaArrowRight size={70} />
+            </div>
+        )}
+    </>
+);
+
+WideScreenLayout.propTypes = {
+    scrollContainerRef: PropTypes.object.isRequired,
+    handleClick: PropTypes.func.isRequired,
+    arrowVisible: PropTypes.bool.isRequired,
+};
+
+const SmallScreenLayout = () => (
+    <div className="small-screen-layout">
+        <Row xs={12} className="d-flex flex-column overflow-auto mb-3">
+            <MostPlayedSongs timeRange="short_term" />
+        </Row>
+        <Row xs={12} className="d-flex flex-column overflow-auto mb-3">
+                <MostPlayedSongs timeRange="medium_term" />
+        </Row>
+        <Row xs={12} className="d-flex flex-column overflow-auto mb-3">
+                <MostPlayedSongs timeRange="long_term" />
+        </Row>
+        <Row xs={12} className="d-flex flex-column overflow-auto mb-3">
+                <MostPlayedArtists timeRange="short_term" />
+        </Row>
+        <Row xs={12} className="d-flex flex-column overflow-auto mb-3">
+                <MostPlayedArtists timeRange="medium_term" />
+        </Row>
+        <Row xs={12} className="d-flex flex-column overflow-auto mb-3">
+                <MostPlayedArtists timeRange="long_term" />
+        </Row>
+    </div>
+);
 
 const TopsPage = () => {
     const [arrowVisible, setArrowVisible] = useState(true);
@@ -35,31 +95,16 @@ const TopsPage = () => {
 
     return (
         <CommonLayout>
-            <div ref={scrollContainerRef} className="scroll-container d-flex overflow-auto no-padding">
-                <Col xs={12} md={4} className="d-flex flex-column overflow-auto mb-3 mb-md-0">
-                    <MostPlayedSongs timeRange="short_term" />
-                </Col>
-                <Col xs={12} md={4} className="d-flex flex-column overflow-auto mb-3 mb-md-0">
-                    <MostPlayedSongs timeRange="medium_term" />
-                </Col>
-                <Col xs={12} md={4} className="d-flex flex-column overflow-auto mb-3 mb-md-0">
-                    <MostPlayedSongs timeRange="long_term" />
-                </Col>
-                <Col xs={12} md={4} className="d-flex flex-column overflow-auto mb-3 mb-md-0">
-                    <MostPlayedArtists timeRange="short_term" />
-                </Col>
-                <Col xs={12} md={4} className="d-flex flex-column overflow-auto mb-3 mb-md-0">
-                    <MostPlayedArtists timeRange="medium_term" />
-                </Col>
-                <Col xs={12} md={4} className="d-flex flex-column overflow-auto">
-                    <MostPlayedArtists timeRange="long_term" />
-                </Col>
+            <div className="d-none d-md-block">
+                <WideScreenLayout
+                    scrollContainerRef={scrollContainerRef}
+                    handleClick={handleClick}
+                    arrowVisible={arrowVisible}
+                />
             </div>
-            {arrowVisible && (
-                <div className="arrow-right" onClick={handleClick}>
-                    <FaArrowRight size={70} />
-                </div>
-            )}
+            <div className="d-block d-md-none">
+                <SmallScreenLayout />
+            </div>
         </CommonLayout>
     );
 };
